ced906fbdae86190df08cc4cb563b6eb1d68c383
Dustin Younse <milsyobtaf@gmail.com>
2026-02-23 11:28:01 -0500

    Update logging output

--- scripts/monitorpower.sh
+++ scripts/monitorpower.sh
@@ -7,10 +7,6 @@ output="" # Output message for the console
 dayTimeCommand="sudo pinctrl 17 op dl" # The command we want to run to start the day
 nightTimeCommand="sudo pinctrl 17 op dh" # The command we want to run to end the day
 
-#echo $startTime
-#echo $endTime
-#echo $currentTime
-
 if [[ $currentTime -ge $startTime && $currentTime -le $endTime ]];
   then
   output="It's daytime! Let's turn on the lights."
@@ -20,4 +16,8 @@ else
   output="It's nighttime! Let's turn off the lights."
 fi
 
+echo "=^.^= =^.^= =^.^= =^.^="
+echo $startTime
+echo $endTime
+echo $currentTime
 echo $output
